68ad7e742a7352adb4faa9886dba26c2ed78f0c8
haili <yaohaili@moqing.com>
2023-03-30 12:04:01 +0800

    add checkString on UrlStatus

--- demo/URLCheckStatus.php
+++ demo/URLCheckStatus.php
@@ -4,6 +4,7 @@ use Lit\DevOps\constant\HttpComparison;
 
 include(dirname(__DIR__) . "/vendor/autoload.php");
 
+
 //构建http检测
 
 //$data = \Lit\DevOps\URL::checkStatus($urls);
@@ -32,3 +33,11 @@ foreach ($data as $urlMapper) {
         var_dump($urlMapper->url . " 状态码异常， 返回状态码是: " . $urlMapper->http_code);
     }
 }
+
+//http返回Body 关键字检查
+$url = "https://m.neo-story.com/web2app/s_book_previous_gray/e00a16004dac095ce26542b2adb4c618?ishm=true&pid=facebook&ndl_pid=facebook&ndl_campaign_id=test11&ndl_ad_id=test222&ndl_adset_id=test333&ndl_act_id=1576838949452043&ndl_book_id=130862&cpp=ios&lang=zh-cn";
+$strKeys = "TimeStamp=1680085914041";
+
+$data = \Lit\DevOps\URL::checkString($url, $strKeys);
+
+var_dump($data->getInsert());
