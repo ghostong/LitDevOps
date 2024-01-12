7699149d5434e80750aae60d9352fbcfa1327154
litong <ghostong@126.com>
2024-01-12 23:47:08 +0800

    修改删除文件返回值

--- src/source/FilesClean.php
+++ src/source/FilesClean.php
@@ -7,23 +7,48 @@ use Lit\Utils\LiFileOperator;
 class FilesClean
 {
 
+    /**
+     * 删除X天之前的文件
+     * @date 2024/1/12
+     * @param $dirs
+     * @param $expireDay
+     * @return \Generator
+     * @author litong
+     */
     public function byDays($dirs, $expireDay) {
         return $this->byHours($dirs, $expireDay * 24);
     }
 
+    /**
+     * 删除X小时之前的文件
+     * @date 2024/1/12
+     * @param $dirs
+     * @param $expireHour
+     * @return \Generator
+     * @author litong
+     */
     public function byHours($dirs, $expireHour) {
         return $this->byMinutes($dirs, $expireHour * 60);
     }
 
+    /**
+     * 删除X分钟之前的文件
+     * @date 2024/1/12
+     * @param $dirs
+     * @param $expireMinute
+     * @return \Generator
+     * @author litong
+     */
     public function byMinutes($dirs, $expireMinute) {
         $cleanTime = time() - $expireMinute * 60;
         foreach ($dirs as $dir) {
             LiFileOperator::listFilesByTime($dir, LiFileOperator::CTIME, function ($fileTime, $realpath) use ($cleanTime) {
                 if ($fileTime < $cleanTime) {
                     unlink($realpath);
+                    yield $realpath;
                 }
             });
         }
-        return true;
+        yield;
     }
 }
