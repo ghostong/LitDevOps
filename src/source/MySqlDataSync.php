88e5e8328d4afb9ec7fe2fbc8b4f4ce346f982a9
litong <ghostong@126.com>
2024-06-22 16:38:41 +0800

    3

--- src/source/MySqlDataSync.php
+++ src/source/MySqlDataSync.php
@@ -13,20 +13,21 @@ class MySqlDataSync
             $this->copyData($config);
         }
     }
-
-
+    
     protected function copyData(MySqlDataSyncMapper $config) {
         $query = $config->fromPdoConn->query("select * from `{$config->toDatabase}`.`{$config->tableName}`");
         $config->toPdoConn->beginTransaction();
         try {
             $config->toPdoConn->query("delete from `$config->toDatabase`.`$config->tableName`");
+            $sql = [];
             while ($row = $query->fetch(\PDO::FETCH_ASSOC)) {
-                $columns = implode(", ", array_keys($row));
-                $values = implode(", ", array_map(function ($value) use ($config) {
-                    return is_null($value) ? "NULL" : $config->toPdoConn->quote($value);
-                }, array_values($row)));
-                $sql = "INSERT INTO `{$config->toDatabase}`.`{$config->tableName}` ($columns) VALUES ($values)";
-                $config->toPdoConn->exec($sql);
+                $sql[] = $this->makeSql($row, $config);
+                if (count($sql) >= 50) {
+                    $config->toPdoConn->exec(implode(";", $sql));
+                }
+            }
+            if (count($sql) > 0) {
+                $config->toPdoConn->exec(implode(";", $sql));
             }
             if ($config->toPdoConn->inTransaction()) {
                 $config->toPdoConn->commit();
@@ -35,7 +36,14 @@ class MySqlDataSync
             $config->toPdoConn->rollBack();
             throw $exception;
         }
+    }
 
+    protected function makeSql($row, $config) {
+        $columns = implode(", ", array_keys($row));
+        $values = implode(", ", array_map(function ($value) use ($config) {
+            return is_null($value) ? "NULL" : $config->fromPdoConn->quote($value);
+        }, array_values($row)));
+        return "insert into `{$config->toDatabase}`.`{$config->tableName}` ($columns) values ($values)";
     }
 
 }
